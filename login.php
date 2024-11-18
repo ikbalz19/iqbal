135a89900dbf1d472f6e59555d0bf10b17ab21ab
ikbalz19 <miqbalnugraha0112@gmail.com>
2024-11-18 11:44:59 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script
+        src="https://kit.fontawesome.com/64d58efce2.js"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="app1.css" />
+    <title>Sign in & Sign up Form | @kntl</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="forms-container">
+            <div class="signin-signup">
+                <h2 class="title">Sign in</h2>
+                <button class="btn transparent" id="sign-in-btn">
+                    Sign in
+                </button>
+            </div>
+            <img src="img/register.svg" class="image" alt="" />
+        </div>
+    </div>
+    </div>
+    <script src="app.js"></script>
+</body>
+
+</html>
